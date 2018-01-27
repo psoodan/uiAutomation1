707ac2d1d7a830152f9a6d33da968383ea02a698
psoodan <psoodan@CSCINDAH503788.asiapac.globalcsc.net>
2018-01-27 13:59:14 +0530

    Initial checkin of uiAutomation1

---
+++ uiAutomation1/src/test/java/com/test/automation1/uiAutomation1/VerifyTitle.java
@@ -0,0 +1,21 @@
+package com.test.automation1.uiAutomation1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class VerifyTitle {
+	WebDriver driver;
+	
+	@Test
+	
+	public void test123(){
+		
+		System.setProperty("webdriver.chrome.driver", "D:/Drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://learn-automation.com/");
+		Assert.assertTrue(driver.getTitle().contains("Selenium"));
+	}
+
+}
